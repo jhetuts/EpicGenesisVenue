87410a9f6b199408a638815feb4500d8dd32b3dc
Alejandro H. Cartojano Jr <alejandro.cartojano@gmail.com>
2021-05-10 18:07:03 +0800

    Added chat button

--- global/scripts/logout.js
+++ global/scripts/logout.js
@@ -1,32 +1,34 @@
 window.addEventListener('DOMContentLoaded', function () {
   const user = document.querySelector('#user')
+  const logout = document.querySelector('#logout')
+  const chat = document.querySelector('#chat')
 
   if (user) {
     user.addEventListener('click', function (e) {
       e.preventDefault()
-      const logout = document.querySelector('#logout')
-      const chat = document.querySelector('#chat')
 
       const userDetails = document.querySelector('.user-details')
       if (userDetails) {
         userDetails.classList.toggle('active')
       }
+    })
+  }
 
-      if (logout) {
-        logout.addEventListener('click', function (e) {
-          e.preventDefault()
-          window.GEVME.utils.logoutUser()
-        })
-      }
+  if (logout) {
+    logout.addEventListener('click', function (e) {
+      e.preventDefault()
+      window.GEVME.utils.logoutUser()
+    })
+  }
 
-      if (chat) {
-        const GevmeChatIframe = document.querySelector('#GevmeChat')
-        if (GevmeChatIframe) {
-          GevmeChatIframe.contentWindow.postMessage(
-            JSON.stringify({ action: 'open', type: 'chat' }),
-            '*',
-          )
-        }
+  if (chat) {
+    chat.addEventListener('click', function () {
+      const GevmeChatIframe = document.querySelector('#GevmeChat')
+      if (GevmeChatIframe) {
+        GevmeChatIframe.contentWindow.postMessage(
+          JSON.stringify({ action: 'open', type: 'chat' }),
+          '*',
+        )
       }
     })
   }
